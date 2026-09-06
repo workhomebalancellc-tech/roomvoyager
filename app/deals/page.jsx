287b6d3571b5da464d0b87c4504133ca24b4c93a
Alyse <workhomebalancellc@gmail.com>
2026-09-05 21:55:43 -0400

    Auto-deploy 2026-09-05 21:55

--- app/deals/page.jsx
+++ app/deals/page.jsx
@@ -44,8 +44,7 @@ const DOMESTIC = [
     city: "Nashville",
     img: "https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?w=600&h=900&fit=crop&auto=format",
     link: "/deals/nashville",
-    liveDate: "2026-08-29",
-    weekEnd: "2026-09-04",
+    locked: true,
   },
   {
     city: "New Orleans",
@@ -72,8 +71,7 @@ const INTERNATIONAL = [
     city: "Punta Cana",
     img: "/Deals/17/puntacana_destination.jpg",
     link: "/deals/punta-cana",
-    liveDate: "2026-08-22",
-    weekEnd: "2026-08-28",
+    locked: true,
   },
   {
     city: "Cancún",
